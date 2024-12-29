58a28c3cd8beadf19b9597a61aa1eda52f97e4f4
Ankit Singhal <itforankit@gmail.com>
2024-12-29 09:43:29 +0530

    playground and othher changes

--- playground.py
+++ playground.py
@@ -1,14 +1,26 @@
-from phi.agent import Agent
-#from phi.model.yfinance import YahooFinanceModel
-from phi.model.groq import Groq
-from phi.tools.yfinance import YFinanceTools
-from phi.tools.duckduckgo import DuckDuckGo
 import openai
-
-import os
+from phi.agent import Agent
+import phi.api
+from phi.model.openai import OpenAIChat
+from phi.tools.duckduckgo import DuckDuckGo
+from phi.tools.yfinance import YFinanceTools
 from dotenv import load_dotenv
+from phi.model.groq import Groq
+import os
+import phi
+from phi.playground import Playground,serve_playground_app
+from phi.utils import *
+from phi.api import *
+#load environment variables from .env file
 load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# agent = Agent(
+#     # Note: 'llm' is now 'model' and 'model' is now 'id'
+#     model=OpenAIChat(id="gpt-4o"),
+# )
+
+phi.api=os.getenv("PHI_API_KEY")
+#openai.api_key = os.getenv("OPENAI_API_KEY")
 
 ## web search Agent
 web_search_agent=Agent(
@@ -35,12 +47,14 @@ financial_agent=Agent(
     markdown=True
 )
 
-multi_ai_agent=Agent(
-    team=[web_search_agent, financial_agent],
-    instructions=["Always include sources","Use tables to display the data"],
-    show_tool_calls=True,
-    markdown=True
-)
+# multi_ai_agent=Agent(
+#     team=[web_search_agent, financial_agent],
+#     instructions=["Always include sources","Use tables to display the data"],
+#     show_tool_calls=True,
+#     markdown=True
+# )
 
-multi_ai_agent.print_response("Summarize analyst recommendation and share the latest news for NVDA",
-                              stream=True)
+app=Playground(agents=[financial_agent, web_search_agent]).get_app()
+
+if __name__=="__main__":
+    serve_playground_app("playground:app", reload=True)
